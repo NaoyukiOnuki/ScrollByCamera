545eae0dc06e8838db5fc188e91a7edb5d83efb3
onuki <rcpxg844@yahoo.co.jp>
2017-06-28 14:57:25 +0900

    refactoring

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/scrollbycamera/MyImgproc.java
@@ -0,0 +1,77 @@
+package jp.ac.titech.itpro.sdl.scrollbycamera;
+
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.imgproc.Moments;
+
+/**
+ * Created by onuki on 2017/06/27.
+ */
+
+public class MyImgproc {
+    public static Moments contourMoments(MatOfPoint contour )
+    {
+        Moments m = new Moments();
+        int lpt = contour.checkVector(2);
+        boolean is_float = true;//(contour.depth() == CvType.CV_32F);
+        Point[] ptsi = contour.toArray();
+//PointF[] ptsf = contour.toArray();
+
+        //CV_Assert( contour.depth() == CV_32S || contour.depth() == CV_32F );
+
+        if( lpt == 0 )
+            return m;
+
+        double a00 = 0, a10 = 0, a01 = 0;
+        double xi, yi, xi2, yi2, xi_1, yi_1, dxy, xii_1, yii_1;
+
+
+        {
+            xi_1 = ptsi[lpt-1].x;
+            yi_1 = ptsi[lpt-1].y;
+        }
+
+        for( int i = 0; i < lpt; i++ )
+        {
+
+            {
+                xi = ptsi[i].x;
+                yi = ptsi[i].y;
+            }
+
+            xi2 = xi * xi;
+            yi2 = yi * yi;
+            dxy = xi_1 * yi - xi * yi_1;
+            xii_1 = xi_1 + xi;
+            yii_1 = yi_1 + yi;
+
+            a00 += dxy;
+            a10 += dxy * xii_1;
+            a01 += dxy * yii_1;
+            xi_1 = xi;
+            yi_1 = yi;
+        }
+        float FLT_EPSILON = 1.19209e-07f;
+        if( Math.abs(a00) > FLT_EPSILON )
+        {
+            double db1_2, db1_6;
+
+            if( a00 > 0 )
+            {
+                db1_2 = 0.5;
+                db1_6 = 0.16666666666666666666666666666667;
+            }
+            else
+            {
+                db1_2 = -0.5;
+                db1_6 = -0.16666666666666666666666666666667;
+            }
+
+            // spatial moments
+            m.m00 = a00 * db1_2;
+            m.m10 = a10 * db1_6;
+            m.m01 = a01 * db1_6;
+        }
+        return m;
+    }
+}
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/scrollbycamera/Vector2D.java
@@ -0,0 +1,84 @@
+package jp.ac.titech.itpro.sdl.scrollbycamera;
+
+import static java.lang.Math.*;
+
+/**
+ * Created by onuki on 2017/06/20.
+ */
+
+public class Vector2D {
+
+    double x;
+    double y;
+
+    Vector2D(double ix, double iy) {
+        x = ix;
+        y = iy;
+    }
+
+    Vector2D plus(Vector2D v) {
+        return new Vector2D(x+v.x, y+v.y);
+    }
+    Vector2D minus(Vector2D v) {
+        return new Vector2D(x-v.x, y-v.y);
+    }
+    Vector2D mult(double d) {
+        return new Vector2D(x*d, y*d);
+    }
+    Vector2D div(double d) {
+        return new Vector2D(x/d, y/d);
+    }
+    double product(Vector2D v) {
+        return x*v.x + y*v.y;
+    }
+    Vector2D normalization() {
+        return new Vector2D(x/length(), y/length());
+    }
+
+    double length() {
+        return sqrt(x*x + y*y);
+    }
+    double angle() {
+        if (x == 0) {
+            if (y > 0) return PI/2;
+            else return PI*3/2;
+        } else {
+            double angle = atan(y/x);
+            if (x < 0) return angle + PI;
+            else return angle;
+        }
+    }
+
+    // 垂直なベクトル
+    Vector2D vertical() {
+        return Vector2D.polarVector(length(), angle()+PI/2);
+    }
+
+    static Vector2D[] dualBasis(Vector2D e1, Vector2D e2) {
+        Vector2D[] ret = new Vector2D[2];
+        Vector2D v1 = e2.vertical();
+        Vector2D v2 = e1.vertical();
+
+        double p1 = v1.product(e1);
+        double p2 = v2.product(e2);
+
+        ret[0] = v1.div(p1);
+        ret[1] = v2.div(p2);
+
+        return ret;
+    }
+
+    // a = decompose[0], b = decompose[1]
+    // input = a*e1 + b*e2
+    static double[] decompose(Vector2D input, Vector2D e1, Vector2D e2) {
+        Vector2D[] dual = dualBasis(e1, e2);
+        double[] ret = new double[2];
+        ret[0] = input.product(dual[0]);
+        ret[1] = input.product(dual[1]);
+        return ret;
+    }
+
+    static Vector2D polarVector(double r, double d) {
+        return new Vector2D(r*cos(d), r*sin(d));
+    }
+}
